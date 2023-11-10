4fccc8fe02eedbbfdc0e3e7f924c6953523c9f4a
Diego <diegomdop@bol.com.br>
2023-11-09 21:59:04 -0300

    alteracao no app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         <strong>Olá meu nome é Diego.</strong> 
         </p>
         <a
           className="App-link"
